e0bf35ab1842f647180ecf388a31caaa0df0e792
Darya Sakalova <Darya.Sakalova@thomsonreuters.com>
2017-04-10 17:10:28 +0300

    Added unit test.

---
+++ src/test/java/com/epam/mentoring/webservices/GeneralTest.java
@@ -0,0 +1,13 @@
+package com.epam.mentoring.webservices;
+
+import static org.junit.Assert.assertNull;
+
+import org.junit.Test;
+
+public class GeneralTest {
+
+	@Test
+	public void test() {
+		assertNull(null);
+	}
+}
