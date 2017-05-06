1d8455b3ea32f1e72b84e0292bcbac12e5c3b56e
Darya Sakalova <Darya.Sakalova@thomsonreuters.com>
2017-05-06 16:34:43 +0300

    added logging

--- main/src/main/java/com/epam/mentoring/webservices/dao/UserDefaultDAO.java
+++ main/src/main/java/com/epam/mentoring/webservices/dao/UserDefaultDAO.java
@@ -4,6 +4,7 @@ import java.sql.SQLException;
 
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.stat.Statistics;
 
 import com.epam.mentoring.webservices.bean.User;
 import com.epam.mentoring.webservices.exception.FileUploadException;
@@ -21,11 +22,15 @@ public class UserDefaultDAO extends BeanDAO<User> implements UserDAO {
 
 		try {
 			Session session = sessionFactory.getCurrentSession();
+		//	Statistics stats = sessionFactory.getStatistics();
+			
 			Transaction transaction = session.beginTransaction();
 			User bean = (User) session.get(User.class, beanID);
 			bytes = bean.getPhoto().getBytes(1, (int) bean.getPhoto().length());
 			session.flush();
 			transaction.commit();
+			
+		//	stats.logSummary();
 		} catch (SQLException e) {
 			throw new FileUploadException(e);
 		}
